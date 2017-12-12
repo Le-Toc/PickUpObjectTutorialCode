cdf2c8ce9aeb32831c492c3249a9402779ed5b58
TOC <thomash1924@gmail.com>
2017-12-12 04:25:47 +0000

    Finished The Second Tutorial
    I followed the second tutorial and created an interactable object.  In the current the state the player will view the object as an interactable and move towards it then the object is right clicked.  The player will stop some distance from it.  The space for the interactactable object can be resized and moved.

--- RPGProject3/Assets/Scripts/PlayerController.cs
+++ RPGProject3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public Interactable focus;
+
     public LayerMask movementMask;
 
     Camera cam;
@@ -30,7 +32,7 @@ public class PlayerController : MonoBehaviour
             {
                 motor.MoveToPoint(hit.point);
 
-                // Stop focusing any objects
+                RemoveFocus();
             }
         }
 
@@ -41,9 +43,35 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100))
             {
-                // Check if we hit an interactable
-                // If we did set it as our focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable != null)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus != focus)
+        {
+            if(focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
